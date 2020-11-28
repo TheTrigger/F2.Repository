cff123851d46b7ec3812cecab21f79ca391a231a
Fabio <f.fabrizi91@gmail.com>
2020-11-28 12:06:24 +0100

    WIP: IDbContextScope

---
+++ Oibi.Repository/Interfaces/IDbContextScope.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Oibi.Repository.Interfaces
+{
+	public interface IDbContextScope
+	{
+		/// <inheritdoc cref="Microsoft.EntityFrameworkCore.DbContext.SaveChangesAsync(CancellationToken)"/>
+		Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+
+		/// <inheritdoc cref="Microsoft.EntityFrameworkCore.DbContext.SaveChangesAsync(bool, CancellationToken)"/>
+		Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken);
+	}
+}

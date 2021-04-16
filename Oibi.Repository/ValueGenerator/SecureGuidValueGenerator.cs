7c7784bc674acbb6ac33c6c7f0f2c29afd6b9f3f
Fabio Fabrizi <f.fabrizi91@gmail.com>
2021-04-17 01:02:34 +0200

    ValueGenerators DateTime and Guid

---
+++ Oibi.Repository/ValueGenerator/SecureGuidValueGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Oibi.Repository.ValueGenerator
+{
+	public class DateTimeValueGenerator : ValueGenerator<DateTime>
+	{
+		public override bool GeneratesTemporaryValues => false;
+
+		public override DateTime Next([NotNullAttribute] EntityEntry entry)
+		{
+			return DateTime.UtcNow;
+		}
+	}
+}

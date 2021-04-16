9eef9f13dd3c5ffb5a623cf29b3c1f2a64956e40
Fabio Fabrizi <f.fabrizi91@gmail.com>
2021-04-17 01:03:49 +0200

    more test

--- Oibi.Repository.Tests/TestControllerApi.cs
+++ Oibi.Repository.Tests/TestControllerApi.cs
@@ -1,7 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using Oibi.Repository.Demo;
+using Oibi.Repository.Demo.Models;
 using Oibi.Repository.Demo.Repositories;
 using Oibi.TestHelper;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -19,16 +22,46 @@ namespace Oibi.Repository.Tests
 		}
 
 		[Fact]
+		public async Task CreateExampleDataIfNotExists()
+		{
+			var a1 = new Author { Name = "William Shakespeare" };
+			var a2 = new Author { Name = "Oibi.dev" };
+
+			var entity = _libraryScope.AuthorRepository.Create(a1);
+			_libraryScope.AuthorRepository.Create(a2);
+
+			var affectedRows = await _libraryScope.SaveChangesAsync();
+			Assert.Equal(2, affectedRows);
+
+			var b1 = new Book { Title = "Hamlet", Isbn = "1234567890123" };
+			var b2 = new Book { Title = "King Lear", Isbn = "0987654321045" };
+			var b3 = new Book { Id = Guid.NewGuid(), Title = "Random Othello w/ no authors" };
+
+			_libraryScope.BookRepository.Create(b1);
+			_libraryScope.BookRepository.Create(b2);
+			_libraryScope.BookRepository.Create(b3);
+
+			a1.Books = new List<Book>() { b1, b2 };
+			a2.Books = new List<Book>() { b2, b3 };
+
+			affectedRows = await _libraryScope.SaveChangesAsync();
+			Assert.NotEqual(default, affectedRows);
+		}
+
+		[Fact]
 		public async Task TestLibrary()
 		{
-			var results = await _libraryScope.AuthorRepository.ToListAsync();
+			var a1 = new Author { Name = "William Shakespeare" };
+			var entity = _libraryScope.AuthorRepository.Create(a1);
+			var results = await _libraryScope.AuthorRepository.Include(i => i.Books)
+				.ToListAsync();
 
 			Assert.NotEmpty(results);
 
 			foreach (var author in results)
 			{
 				Assert.NotEmpty(author.Name);
-				Assert.NotEmpty(author.BookAuthors);
+				Assert.NotEmpty(author.Books);
 			}
 		}
 

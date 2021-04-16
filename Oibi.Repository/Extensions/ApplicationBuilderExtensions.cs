c78a07afda2487d310d71b2a83931241646042c0
Fabio Fabrizi <f.fabrizi91@gmail.com>
2021-04-17 01:00:29 +0200

    auto migration extension

---
+++ Oibi.Repository/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Oibi.Repository.Extensions
+{
+	public static class ApplicationBuilderExtensions
+	{
+		/// <summary>
+		/// Automatically check migration is up to date. Only relational database is supported. <see cref="Microsoft.EntityFrameworkCore.Relational"/>
+		/// </summary>
+		/// <typeparam name="TDbContext"></typeparam>
+		/// <param name="applicationBuilder"></param>
+		public static IApplicationBuilder UseDatabaseMigration<TDbContext>(this IApplicationBuilder applicationBuilder) where TDbContext : DbContext
+		{
+			using var serviceScope = applicationBuilder.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope();
+			var context = serviceScope.ServiceProvider.GetService<TDbContext>();
+			context.Database.Migrate();
+
+			return applicationBuilder;
+		}
+	}
+}

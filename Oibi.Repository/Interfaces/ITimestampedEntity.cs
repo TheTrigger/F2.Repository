e1d694ca3e32536de42251fffb2ea9d500be6374
Fabio Fabrizi <f.fabrizi91@gmail.com>
2021-04-16 17:04:29 +0200

    new interface

---
+++ Oibi.Repository/Interfaces/ITimestampedEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Oibi.Repository.Interfaces
+{
+	public interface ITimestampedEntity
+	{
+		public DateTime CreatedAt { get; set; }
+		public DateTime UpdatedAt { get; set; }
+	}
+}
